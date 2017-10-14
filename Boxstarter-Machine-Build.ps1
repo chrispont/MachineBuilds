dc7ca89cdca9df35f12005feab2e744ad5ae7af0
Chris Pont <chris.pont@ijyi.com>
2017-10-14 17:07:09 +0100

    Streamline install (only essential dev features)

--- Boxstarter-Machine-Build.ps1
+++ Boxstarter-Machine-Build.ps1
@@ -22,7 +22,6 @@ choco install microsoft-teams -y
 
 #Other dev tools
 choco install fiddler4 -y
-choco install nodejs.install -y
 choco install gitextensions -y
 choco install kdiff3 -y
 choco install docker -y
@@ -38,14 +37,7 @@ choco install webpi -y
 choco install resharper-platform -y
 
 #Other essential tools
-choco install winrar -y
-choco install adobereader -y
-choco install skype -y
-choco install dropbox -y
-choco install windirstat -y
-choco install virtualbox -y
 choco install jdk8 -y
-choco install adobe-creative-cloud -y
 choco install keepass -y
 choco install visualstudiocode -y
 choco install wireshark -y
@@ -53,7 +45,6 @@ choco install slack -y
 choco install silverlight -y
 choco install filezilla -y
 
-choco install Microsoft-Hyper-V-All -source windowsFeatures
 choco install IIS-WebServerRole -source windowsfeatures
 choco install IIS-HttpCompressionDynamic -source windowsfeatures
 choco install IIS-ManagementScriptingTools -source windowsfeatures
